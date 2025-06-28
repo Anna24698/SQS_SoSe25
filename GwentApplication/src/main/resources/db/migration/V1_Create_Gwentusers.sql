4e083871f259dee94111a66eec58f3ed05fea753
Anna Holzner <anna.holzner@stud.th-rosenheim.de>
2025-06-28 12:47:22 +0200

    remove migration

--- GwentApplication/src/main/resources/db/migration/V1_Create_Gwentusers.sql
+++
@@ -1,9 +0,0 @@
-/*use GwentDB;
-
-drop table  IF EXISTS Gwentusers;
-
-create OR REPLACE table Gwentusers(
-    id LONG AUTO_INCREMENT ,
-    username VARCHAR(200) not null,
-    password VARCHAR(60) not null
-);*/
--- GwentApplication/src/main/resources/db/migration/V1_create_Roles.sql
+++
@@ -1,8 +0,0 @@
-/*use GwentDB;
-
-drop table  IF EXISTS roles;
-
-create OR REPLACE table roles(
-    id BIGINT not null ,
-    name VARCHAR(25) not null
-);*/
